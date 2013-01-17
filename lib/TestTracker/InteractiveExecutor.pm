fd6235d589eb6ed4086e37cc850bbd18848ce44d
Nathan Nutter <iam@nnutter.com>
2013-01-16 21:15:08 -0600

    redirect output
    - Redirect test's STDOUT so we can filter LSF job submitted messages.
    - Redirect test's STDERR, and only show on on error, so we can keep
      prove's output clean.

--- lib/TestTracker/InteractiveExecutor.pm
+++ lib/TestTracker/InteractiveExecutor.pm
@@ -3,11 +3,32 @@ package TestTracker::InteractiveExecutor;
 use strict;
 use warnings;
 
-use autodie qw(system);
+use IPC::Open3;
+use Symbol qw(gensym);
 
 sub main {
     my ($exec, $test_name) = @_;
-    system(qq(bsub -I -q interactive "$exec" "$test_name"));
+    my $job_regex = '^Job <[0-9]+> is submitted to queue <short>.$';
+
+    # This redirects the first job submitted message to STDERR which is the
+    # message for the bsub of the test itself but not any that might occur
+    # during the test. Otherwise there will be a lot of these messages.
+    my $awk = qq|{ if (/$job_regex/ && count++ == 0) print > "/dev/stderr"; else print }|;
+    my $cmd = qq(bash -c "set -o pipefail; bsub -I -q short '$exec' '$test_name' | awk '$awk'");
+
+    # Child's STDERR is "trapped" into CHLDERR and only shown if the child
+    # exits non-zero, e.g. if it crashes. This keeps the output clean but still
+    # helps with debugging.
+    my $pid = open3(gensym, ">&STDOUT", \*CHLDERR, $cmd);
+    my @stderr = <CHLDERR>;
+    waitpid($pid, 0);
+    if($? != 0) {
+        print STDERR "\n\n";
+        print STDERR "***** PROVE EXECUTOR STDERR *****\n";
+        print STDERR @stderr;
+        print STDERR '*' x 33, "\n";
+        print STDERR "\n";
+    }
 }
 
 1;
