8217a421cdaffdb4d782f191faa9a888a441af9e
David Morton <davidlmorton@gmail.com>
2013-05-20 16:55:14 -0500

    Memoize git_base_dir

--- lib/TestTracker.pm
+++ lib/TestTracker.pm
@@ -12,6 +12,8 @@ use List::MoreUtils qw(uniq);
 use Pod::Usage;
 use TestTracker::Config;
 use autodie qw(:system);
+use Memoize "memoize";
+use Cwd "getcwd";
 
 sub db_connection {
     my %config = TestTracker::Config::load();
@@ -21,12 +23,20 @@ sub db_connection {
     return $dbh;
 }
 
-sub git_base_dir {
+sub _git_base_dir {
+    my ($cwd) = @_; # used for memoization
+
     my $git_dir = capture(qq(git rev-parse --git-dir));
     chomp $git_dir;
     my $abs_git_dir = File::Spec->rel2abs($git_dir);
     my $git_base_dir = (File::Spec->splitpath($abs_git_dir))[1];
     return $git_base_dir;
+}
+memoize('_git_base_dir');
+
+sub git_base_dir {
+    my $cwd = getcwd();
+    return _git_base_dir($cwd);
 }
 
 sub changed_files_from_git {
