c92451d40ab6e4853817bffd35742a0d0c610315
Shin Leong <sleong@wustl.edu>
2016-04-20 09:43:43 -0500

    refactor the interactive executor module and add a test.

--- lib/TestTracker/InteractiveExecutor.pm
+++ lib/TestTracker/InteractiveExecutor.pm
@@ -3,36 +3,89 @@ package TestTracker::InteractiveExecutor;
 use strict;
 use warnings;
 
-use IPC::Open3;
-use Symbol qw(gensym);
+use IPC::Run (qw(run));
+
+=head2 main
+
+Executor main function.
+
+PARAM: $exec, $test_name
+RETURNS: <none>
+
+=cut
 
 sub main {
-    my ($exec, $test_name) = @_;
+    @_ == 2 or die 'ERROR: Expecting 2 parameters but found ' . scalar(@_) . '.';
+    execute( @_, $ENV{'TEST_TRACKER_LSF_QUEUE'} || 'short' );
+}
 
-    my $queue = $ENV{TEST_TRACKER_LSF_QUEUE} || 'short';
+=head2 execute
 
+Execute the shell command.
+
+Child's STDERR is "trapped" into CHLDERR and only shown if the child
+exits non-zero, e.g. if it crashes. This keeps the output clean but still
+helps with debugging.
+
+PARAMS: $exec, $test_name, $queue
+RETURNS: <none>
+
+=cut
+
+sub execute {
+    my ( $exec, $test_name, $queue ) = @_;
+
+    my ( $stdout, $stderr ) = ('', '');
+    run( construct_bsub_command( $exec, $test_name, $queue ), '<pty<', \undef, '>pty>', \$stdout, '2>', \$stderr )
+        or do {
+        return print_error_message( $exec, $test_name, $?, $stderr, $stdout );
+        };
+    print $stdout;
+
+}
+
+=head2 construct_bsub_command
+
+Construct bsub command.
+
+This redirects the first job submitted message to STDERR which is the
+message for the bsub of the test itself but not any that might occur
+during the test. Otherwise there will be a lot of these messages.
+Be super careful with quoting!
+
+PARAMS: $exec, $test_name, $queue
+RETURNS: $array_ref_of_command
+
+=cut
+
+sub construct_bsub_command {
+    my ( $exec, $test_name, $queue ) = @_;
     my $job_regex = '^Job <[0-9]+> is submitted to queue <' . $queue . '>.$';
 
-    # This redirects the first job submitted message to STDERR which is the
-    # message for the bsub of the test itself but not any that might occur
-    # during the test. Otherwise there will be a lot of these messages.
-    # Be super careful with quoting!
-    my $awk = qq|awk '{ if (/$job_regex/ && count++ == 0) print \\\$0 > \\"/dev/stderr\\"; else print \\\$0; }'|;
-    my $cmd = qq(bash -c "set -o pipefail; bsub -Is -q $queue '$exec' '$test_name' | $awk");
+    my $awk
+        = qq|awk '{ if (/$job_regex/ && count++ == 0) print \$0 > "/dev/stderr"; else print \$0; }'|;
+    return [ 'bash', '-c', "set -o pipefail; bsub -Is -q $queue '$exec' '$test_name' | $awk" ];
+}
 
-    # Child's STDERR is "trapped" into CHLDERR and only shown if the child
-    # exits non-zero, e.g. if it crashes. This keeps the output clean but still
-    # helps with debugging.
-    my $pid = open3(gensym, ">&STDOUT", \*CHLDERR, $cmd);
-    my @stderr = <CHLDERR>;
-    waitpid($pid, 0);
-    if($? != 0) {
-        print STDERR "\n\n";
-        print STDERR "***** $test_name STDERR *****\n";
-        print STDERR @stderr;
-        print STDERR '*' x (length($test_name) + 19), "\n";
-        print STDERR "\n";
-    }
+=head2 print_error_message
+
+Print the error message out to the STDERR.
+
+PARAMS: $exec, $test_name, $error_code, $stderr_message, $stdout_message
+RETURNS: <none>
+
+=cut
+
+sub print_error_message {
+    my ( $exec, $test_name, $error_code, $stderr, $stdout ) = @_;
+    my $error_message = <<EOT;
+***** $test_name STDERR *****
+ERROR CODE: $error_code
+$stderr
+***** $test_name STDOUT *****
+$stdout
+EOT
+    print STDERR ( $error_message, ( '*' x ( length($test_name) + 19 ) ), "\n" );
 }
 
 1;
---
+++ t/unit/TestTracker/InteractiveExecutor.t
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Sub::Override;
+use Try::Tiny;
+
+use_ok('IPC::Run');
+use_ok('TestTracker::InteractiveExecutor');
+
+&test_argument;
+&test_command;
+
+done_testing;
+
+sub test_argument {
+    my @arguments = ( 'what', 'ever', 'work' );
+    for ( 0, 1, 3 ) {
+        eval { TestTracker::InteractiveExecutor::main( @arguments[ 0 .. ( $_ - 1 ) ] ); };
+        like(
+            $@,
+            qr/ERROR: Expecting 2 parameters but found $_\./,
+            "should not have [$_ argument"
+                . ( $_ == 1 ? ''     : 's' ) . '] '
+                . ( $_ > 2  ? 'more' : 'less' )
+                . ' than 2 arguments'
+        );
+    }
+}
+
+sub test_command {
+    my $sub_override = Sub::Override->new();
+    $sub_override->replace(
+        'TestTracker::InteractiveExecutor::run' => sub {
+            is_deeply(
+                $_[0],
+                [   'bash',
+                    '-c',
+                    q{set -o pipefail; bsub -Is -q test 'what' 'ever' | awk '{ if (/^Job <[0-9]+> is submitted to queue <test>.$/ && count++ == 0) print $0 > "/dev/stderr"; else print $0; }'}
+                ],
+                'correct command',
+            );
+        }
+    );
+
+    local $ENV{TEST_TRACKER_LSF_QUEUE} = 'test';
+    TestTracker::InteractiveExecutor::main( 'what', 'ever' );
+
+}
